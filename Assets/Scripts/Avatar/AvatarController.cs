d417b0e0d3d106878f7ee89ef8a97a7b5607305a
Rodrigo Tarriba <rodrigotarriba@gmail.com>
2022-10-11 22:28:45 -0700

    Detecting Ground plane 2

--- Assets/Scripts/Avatar/AvatarController.cs
+++ Assets/Scripts/Avatar/AvatarController.cs
@@ -5,31 +5,54 @@ using UnityEngine;
 /// </summary>
 public class AvatarController : MonoBehaviour
 {
-    [SerializeField] public VRMapping head;
-    [SerializeField] public VRMapping leftHand;
-    [SerializeField] public VRMapping rightHand;
-    [SerializeField] public VRMapping leftFoot;
-    [SerializeField] public VRMapping rightFoot;
+    [SerializeField] public VRHeadMapper headVR;
+    [SerializeField] public VRHandMapper leftHand;
+    [SerializeField] public VRHandMapper rightHand;
+    [SerializeField] public VRFootMapper leftFootVR;
+    [SerializeField] public VRFootMapper rightFootVR;
 
     [SerializeField] private float turnSmoothness;
 
     [SerializeField] Transform ikHead;
     [SerializeField] Vector3 headBodyOffset;//height of character/person
+    [SerializeField] Vector3 anklesHeight;//height of character/person
 
-    [SerializeField] private LowerBodyGroundCheck lowerBodyGroundCheck;
+    private Transform leftFootIK;
+    private Transform rightFootIK;
 
-    //Only if using kinect locally
-    //[SerializeField] private float rightFootHeight;
-    //[SerializeField] private Transform rightFootTarget;
-    //[SerializeField] private Transform kinectSphereRightFootTracker;
-    //[SerializeField] KinectManager kinectManager;
+    [SerializeField] Transform sphere;
 
+
+    /// <summary>
+    /// Raycast to determine how far BELOW ground the foot kinematics must attempt to stick to the ground.
+    /// </summary>
+    [SerializeField] private Vector3 raycastThresholdBelowGround;
+
+    /// <summary>
+    /// Raycast to determine how far ABOVE ground the foot kinematics must attempt to stick to the ground.
+    /// </summary>
+    [SerializeField] private Vector3 raycastThresholdAboveGround;
+
+
+    /// <summary>
+    /// Layer that determines solid objects to be stand-on
+    /// </summary>
+    private int solidSurfaceLayer;
+
+    /// <summary>
+    /// Reference to avatar controller script for this player.
+    /// </summary>
+    private AvatarController avatarController;
+
+    [SerializeField] 
 
     private void Awake()
     {
-        lowerBodyGroundCheck = GetComponent<LowerBodyGroundCheck>();
-    }
+        solidSurfaceLayer = 1 << LayerMask.NameToLayer("SolidSurface"); //layer for collisions with solid surfaces
+        avatarController = GetComponent<AvatarController>();
 
+
+    }
 
     private void Update()
     {
@@ -38,46 +61,115 @@ public class AvatarController : MonoBehaviour
         transform.position = ikHead.position + headBodyOffset;
         transform.forward = Vector3.Lerp(transform.forward, Vector3.ProjectOnPlane(ikHead.forward, Vector3.up).normalized, Time.deltaTime * turnSmoothness);  // We only want the Y axis rotation, instead of veering sides or tilting the head weirdly. - We add a lerp from the previous forward position to allow a level of smoothness and no immediate jitter.
 
-        head.Mapping();
-        leftFoot.Mapping();
-        rightFoot.Mapping();
+        headVR.Mapping(); 
+        leftFootVR.Mapping();
+        rightFootVR.Mapping();
 
+        //Pending implementation of hand tracing through Kinect
+        //leftHand.Mapping();
+        //rightHand.Mapping();
 
+        RaycastHit leftFootHit;
+        RaycastHit rightFootHit;
 
+        if (GroundHitDetection(leftFootVR, out leftFootHit))
+        { 
+            leftFootHit.collider.GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+
+        if (GroundHitDetection(rightFootVR, out rightFootHit))
+        {
+            sphere.position = rightFootHit.point;
+            rightFootHit.collider.GetComponent<MeshRenderer>().material.color = Color.red;
+        }
 
     }
-}
 
+
+
+    public bool GroundHitDetection(VRFootMapper footMapper, out RaycastHit footHit)
+    {
+        //Desired thresehold for sticking foot into ground/solid
+        float raycastMagnitude = Vector3.Distance(raycastThresholdAboveGround, raycastThresholdBelowGround);
+
+        bool isFootDown = Physics.Raycast(footMapper.ikTarget.position - anklesHeight + raycastThresholdBelowGround, Vector3.up, out footHit, raycastMagnitude, solidSurfaceLayer);
+
+        if (isFootDown)
+        {
+            footMapper.GroundRemapping(footHit.point);
+        }
+
+        return isFootDown;
+    }
+
+}
 
 
 /// <summary>
 /// Maps an IK target into the position and rotation of a VR target.
 /// </summary>
 [System.Serializable]
-public class VRMapping : IMapper
+public class VRHeadMapper : IMapper
 {
     //A couple of variables to holding hands and things like that.
     //Transform for the vr headset,. 
 
-    public Transform vrTarget;
-    public Transform ikTarget;
-    public Vector3 trackingPositionOffset;
-    public Vector3 trackingRotationOffset;
+    [SerializeField] public Transform vrTarget;
+    [SerializeField] public Transform ikTarget;
+    [SerializeField] public Vector3 trackingPositionOffset;
+    [SerializeField] public Vector3 trackingRotationOffset;
 
 
-
-    public void Mapping()
+    public Transform Mapping()
     {
+        
         //Map position and rotation of the given vectors
         ikTarget.position = vrTarget.TransformPoint(trackingPositionOffset);
         ikTarget.rotation = vrTarget.rotation * Quaternion.Euler(trackingRotationOffset);
+        return ikTarget;
+
     }
 
 }
 
-
 [System.Serializable]
-public class KinectMapping : IMapper
+public class VRFootMapper : IMapper
+{
+    //A couple of variables to holding hands and things like that.
+    //Transform for the vr headset,. 
+
+    [SerializeField] public Transform vrTarget;
+    [SerializeField] public Transform ikTarget;
+    [SerializeField] public Vector3 trackingPositionOffset;
+    [SerializeField] public Vector3 trackingRotationOffset;
+    [SerializeField] public Transform footVRRotationReference;
+
+    public Transform Mapping()
+    {
+        //Map position and rotation of the given vectors
+        ikTarget.position = vrTarget.TransformPoint(trackingPositionOffset);
+        ikTarget.rotation = vrTarget.rotation * Quaternion.Euler(trackingRotationOffset);
+        return ikTarget;
+    }
+
+    public void GroundRemapping(Vector3 groundHit)
+    {
+        float yRotation = ikTarget.localRotation.eulerAngles.y;
+        ikTarget.rotation = footVRRotationReference.rotation * Quaternion.Euler(trackingRotationOffset);
+        ikTarget.localRotation = Quaternion.Euler(ikTarget.localRotation.eulerAngles.x, yRotation + 10f, ikTarget.localRotation.eulerAngles.z);
+
+    }
+
+
+
+
+}
+
+
+
+//Pending implementation to kinect synchronizatoin
+[System.Serializable]
+public class VRHandMapper : IMapper
 {
     public Transform kinectTarget;
     public Transform ikTarget;
@@ -85,18 +177,18 @@ public class KinectMapping : IMapper
     public Vector3 trackingRotationOffset;
 
 
-    public void Mapping()
+    public Transform Mapping()
     {
         Vector3 currentPos = ikTarget.position;
-
         currentPos.y = kinectTarget.position.y;
-
         ikTarget.position = currentPos;
+        return ikTarget;
     }
 }
 
 
+
 public interface IMapper
 {
-    public void Mapping();
+    public Transform Mapping();
 }
